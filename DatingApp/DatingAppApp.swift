31d482f77a299080a58844eedd4d328775b2f106
vbugrym <vbugrym@lohika.com>
2024-03-12 21:45:43 +0200

    Initial Commit

---
+++ DatingApp/DatingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatingAppApp.swift
+//  DatingApp
+//
+//  Created by vbugrym on 12.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct DatingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
